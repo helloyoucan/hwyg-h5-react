dcf455736b35cf2b9fdcfd3972dfa3cd366ba148
helloyoucan <helloyoucan@163.com>
2019-05-21 17:54:39 +0800

    upload

---
+++ src/containers/Auction/index.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import styles from './styles.module.scss'
+
+export default class Auction extends Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
--- src/containers/Goods/components/Filter/index.tsx
+++ src/containers/Goods/components/Filter/index.tsx
@@ -2,69 +2,104 @@ import React, { Component } from 'react'
 import styles from './styles.module.scss'
 import Icon from '@base/Icon/index'
 interface State {
-    activeFilter: symbol | string
+    showFilterType: symbol | string,
+    priceSort: string
 }
 const FilterType = {
     price: Symbol('price'),
     storage: Symbol('storage'),
     fish: Symbol('fish'),
-    category: Symbol('category'),
+    category: Symbol('category')
 }
+
+const priceSortList = [
+    { text: '默认排序', type: '-1' },
+    { text: '价格降序', type: '0' },
+    { text: '价格升序', type: '1' }
+]
 export default class Filter extends Component {
     state: State = {
-        activeFilter: ''
+        showFilterType: '',
+        priceSort: '-1'
     }
     constructor(props: any) {
         super(props)
         this.handleClickFilterItem = this.handleClickFilterItem.bind(this)
+        this.handleClickPriceSort = this.handleClickPriceSort.bind(this)
     }
     handleClickFilterItem(FilterType: symbol | string) {
-        const activeFilter = this.state.activeFilter
-        if (activeFilter === FilterType) {
+        const showFilterType = this.state.showFilterType
+        if (showFilterType === FilterType) {
             this.setState({
-                activeFilter: ''
+                showFilterType: ''
             })
-        } else if (activeFilter === '') {
+        } else if (showFilterType === '') {
             this.setState({
-                activeFilter: FilterType
+                showFilterType: FilterType
             })
         } else {
             this.setState({
-                activeFilter: ''
+                showFilterType: ''
             }, () => {
                 setTimeout(() => {
                     this.setState({
-                        activeFilter: FilterType
+                        showFilterType: FilterType
                     })
-                }, 400);
+                }, 0);
             })
         }
 
     }
+    handleClickPriceSort(e: React.MouseEvent) {
+        e.persist()
+        const target = e.target as HTMLLIElement
+        if (target.nodeName.toLocaleUpperCase() === 'LI') {
+            this.setState({
+                priceSort: target.getAttribute('data-type'),
+                showFilterType: ''
+            })
+        }
+        e.nativeEvent.stopImmediatePropagation()
+    }
     render() {
-        const activeFilter: symbol | string = this.state.activeFilter
+        const { showFilterType, priceSort } = this.state
+        const priceSortListItem = priceSortList.find(item => item.type === priceSort)
+        const priceSortLabel = priceSort === '-1' ? '价格' : priceSortListItem && priceSortListItem.text
         return (
             <div className={styles.filter}>
-                <div className={activeFilter === FilterType.price ? styles.active : undefined}>
-                    <div className={styles.triangle} onClick={() => { this.handleClickFilterItem(FilterType.price) }}>价格</div>
-                    <div className={styles.content}>
-                        1111111111
+                <div className={showFilterType === FilterType.price ? styles.active : undefined}>
+                    <div
+                        className={[styles.label, styles.triangle].join(' ')}
+                        onClick={() => { this.handleClickFilterItem(FilterType.price) }}>
+                        {priceSortLabel}
+                    </div>
+                    <div
+                        onClick={() => { this.handleClickFilterItem('') }} 
+                        className={styles.content}>
+                        <ul
+                            style={{ height: showFilterType === FilterType.price ? `calc(${priceSortList.length} * ( 80 / 750 ) *100vw)` : 0 }}
+                            className={styles.filterList}
+                            onClick={(e) => { this.handleClickPriceSort(e) }}>
+                            {
+                                priceSortList.map(item => (<li key={item.type} className={priceSort === item.type ? styles.active : undefined} data-type={item.type}>{item.text}</li>))
+                            }
+                        </ul>
                     </div>
                 </div>
-                <div className={activeFilter === FilterType.storage ? styles.active : undefined}>
-                    <div className={styles.triangle} onClick={() => { this.handleClickFilterItem(FilterType.storage) }}>存储类型</div>
+                <div className={showFilterType === FilterType.storage ? styles.active : undefined}>
+                    <div className={[styles.label, styles.triangle].join(' ')} onClick={() => { this.handleClickFilterItem(FilterType.storage) }}>存储类型</div>
                     <div className={styles.content}>
-                        222222222
+
                     </div>
                 </div>
-                <div className={activeFilter === FilterType.fish ? styles.active : undefined}>
-                    <div className={styles.triangle} onClick={() => { this.handleClickFilterItem(FilterType.fish) }}>渔获类型</div>
+                <div className={showFilterType === FilterType.fish ? styles.active : undefined}>
+                    <div className={[styles.label, styles.triangle].join(' ')} onClick={() => { this.handleClickFilterItem(FilterType.fish) }}>渔获类型</div>
                     <div className={styles.content}>
-                        33333333333
+
                     </div>
                 </div>
-                <div className={activeFilter === FilterType.category ? styles.active : undefined}>
-                    <div onClick={() => { this.handleClickFilterItem(FilterType.category) }}><Icon className="icon" icon="icon_classify" />分类</div>
+                <div className={showFilterType === FilterType.category ? styles.active : undefined}>
+                    <div className={[styles.label, styles.triangle].join(' ')} onClick={() => { this.handleClickFilterItem(FilterType.category) }}><Icon className="icon" icon="icon_classify" />分类</div>
                     <div className={styles.content}>
 
                     </div>
