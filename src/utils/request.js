7cae0aa6521a0001ef8cbe8be78efdde219c5c7d
helloyoucan <helloyoucan@163.com>
2019-07-04 17:30:29 +0800

    add proxy

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,11 +3,12 @@ import axios from 'axios'
 // 创建axios实例
 const service = axios.create({
   timeout: 15000, // 请求超时时间15s
-  headers: { 
-    'Cache-Control': 'no-cache', 
+  baseURL: process.env.NODE_ENV === 'production' ? '/graphql' : '',
+  headers: {
+    'Cache-Control': 'no-cache',
     'If-Modified-Since': '0',
-    'Content-Type':'application/graphql'
-   }
+    'Content-Type': 'application/graphql'
+  }
 })
 
 // request拦截器
@@ -16,15 +17,15 @@ service.interceptors.request.use(config =>
   //   config.headers['X-Token'] = getCookies() // 让每个请求携带自定义token 请根据实际情况自行修改
   // }
   config, (error) => {
-  console.log(error) // for debug
-  Promise.reject(error)
-})
+    console.log(error) // for debug
+    Promise.reject(error)
+  })
 
 // respone拦截器
 service.interceptors.response.use(
   (response) => {
     const _schema = response.data._schema
-   if (_schema.code !== 'success') {
+    if (_schema.code !== 'success') {
       return Promise.reject(_schema)
     }
     return response.data
