b33b5ef0a57bf6e58769c0e116bf3d26b07b89e8
helloyoucan <helloyoucan@163.com>
2019-07-10 16:01:41 +0800

    fix issues viewpoint height in chrome and safari

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,9 @@ import * as serviceWorker from './serviceWorker';
 import '@/styles/index.scss'
 // import { Provider } from 'react-redux'
 import Router from '@/router/index'
-ReactDOM.render( 
+ReactDOM.render(
     // <Provider store={store}>
-        <App router={Router}/>,
+    <App router={Router} />,
     // </Provider>,
     document.getElementById('root')
 );
@@ -16,3 +16,14 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+//解决chrome和Safari viewpoint height 会计算地址栏的问题
+const setVh = () => {
+    // 首先获取视窗高度，再乘以1%得到vh单位的值 
+    let vh = window.innerHeight * 0.01; // 把--vh的值设置到文档的根元素中 
+    document.documentElement.style.setProperty('--vh', `${vh}px`);//添加CSS自定义属性级联变量
+}
+
+setVh()
+// 监听resize事件 
+window.addEventListener('resize', setVh);
