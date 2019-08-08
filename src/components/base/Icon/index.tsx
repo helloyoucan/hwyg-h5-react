51986d3bfb2b532e66ce3164e04c0f3a76f5960d
helloyoucan <helloyoucan@163.com>
2019-08-08 10:16:52 +0800

    'fix-icon-size'

--- src/components/base/Icon/index.tsx
+++ src/components/base/Icon/index.tsx
@@ -1,16 +1,16 @@
-import React,{Component} from 'react'
-import styles from './index.module.scss'
-interface Props{
-    className?:String,
-    onClick?:(event:any)=>void,
-    icon?:String
+import React, { Component } from 'react'
+import './index.module.scss'
+interface Props {
+    className?: String,
+    onClick?: (event: any) => void,
+    icon?: String
 }
-export default class Icon extends Component<Props,any>{
-    render(){
-        const icon:any = this.props.icon
-        let {className,onClick}:Props = this.props
-        const classNameList:[String] = [styles.icon]
-        className&&classNameList.push(className)
+export default class Icon extends Component<Props, any>{
+    render() {
+        const icon: any = this.props.icon
+        let { className, onClick }: Props = this.props
+        const classNameList: [String] = ["global_icon"]
+        className && classNameList.push(className)
         return (
             <svg className={classNameList.join(' ')} onClick={onClick} aria-hidden="true">
                 <use xlinkHref={`#${icon}`}></use>
